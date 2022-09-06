d848b00bb2993e682ba3806769b30728113aacf4
zn_dk <qiu5873@gmail.com>
2022-09-06 11:33:26 +0800

    开始vue 3 学习 - cli 搭建完成

--- 220906learn/v3-learn/src/App.vue
+++ 220906learn/v3-learn/src/App.vue
@@ -10,7 +10,11 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  created() {
+    console.log(this)
+  },
+
 }
 </script>
 
--- 220906learn/v3-learn/src/main.js
+++ 220906learn/v3-learn/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//如果需要自定义全局
+const app = createApp({})
+app.config.globalProperties.$test = (msg) => { console.log(msg) }
+
 createApp(App).mount('#app')

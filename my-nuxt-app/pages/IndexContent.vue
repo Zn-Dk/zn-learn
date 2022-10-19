1d5e333dbf3815ab62c7a3bc2354891b039d678e
zn_dk <qiu5873@gmail.com>
2022-10-19 20:09:53 +0800

    nuxt recommit 221019

---
+++ my-nuxt-app/pages/IndexContent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!-- <a-select
+      v-if="options"
+      v-model:value="value"
+      :options="options"
+      mode="multiple"
+      placeholder="Please select"
+      style="width: 200px"
+    >
+    </a-select> -->
+
+    <a-select
+      v-model:value="value"
+      placeholder="Please select"
+      :options="[{ value: 1 }, { value: 2 }]"
+    >
+    </a-select>
+    <!-- <a-select
+      v-model:value="value"
+      style="width: 100%"
+      placeholder="Please select"
+      :options="
+        [...Array(25)].map((_, i) => ({
+          value: (i + 10).toString(36) + (i + 1),
+        }))
+      "
+    >
+    </a-select>
+    <a-select
+      v-model:value="value"
+      mode="multiple"
+      style="width: 100%"
+      placeholder="Please select"
+      :options="
+        [...Array(25)].map((_, i) => ({
+          value: (i + 10).toString(36) + (i + 1),
+        }))
+      "
+    >
+    </a-select> -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const options = Array.from(new Array(10), (item) => {
+  return { value: Math.random().toString(20).slice(-8) }
+})
+let value = ref('')
+</script>
+
+<style lang="scss"></style>
---
+++ my-nuxt-app/pages/IndexHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <a-layout-header>
+    <div class="logo" />
+    <a-menu
+      theme="dark"
+      mode="horizontal"
+      v-model:selectedKeys="selectedKeys"
+      :style="{ lineHeight: '64px' }"
+    >
+      <!-- key 选中后会存入 selectedKeys 数组 -->
+      <template v-for="item in headerList">
+        <template v-if="!item.children">
+          <a-menu-item :key="item.route">
+            {{ item.name }}
+          </a-menu-item>
+        </template>
+        <!-- 存在 children 渲染 submenu  需要自定义一个组件递归方式创建 -->
+        <template v-else> </template>
+      </template>
+    </a-menu>
+    <!-- 切换顶部栏主题 可以做进 vuex/pinia 全局管理 -->
+  </a-layout-header>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+// import SubMenu from '../components/SubMenu.vue'
+
+const headerList = ref([
+  { route: 'index', name: '首页' },
+  { route: 'docs', name: '文档' },
+  {
+    route: 'column',
+    name: '分类',
+    children: [
+      { route: 'javascript', name: 'JavaScript' },
+      { route: 'vue', name: 'Vue' },
+      { route: 'react', name: 'React' },
+    ],
+  },
+  { route: 'chat', name: '留言' },
+  { route: 'about', name: '关于' },
+])
+
+const selectedKeys = ref<string[]>([])
+</script>
+
+<style lang="scss"></style>

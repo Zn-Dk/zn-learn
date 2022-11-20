86333cd6af1b4042113e26fbc386c8c794a75dee
zn_dk <qiu5873@gmail.com>
2022-11-21 00:04:28 +0800

    Update: ts-learn & nginx

--- ts-learn/02-yarn+ts+webpack/src/04函数/类型保护.ts
+++ ts-learn/02-yarn+ts+webpack/src/04函数/类型保护.ts
@@ -7,51 +7,48 @@
 */
 
 interface IBird {
-  fly: boolean
-  sing(): void
+  fly: boolean;
+  sing(): void;
 }
 interface IDog {
-  fly: boolean
-  bark(): void
+  fly: boolean;
+  bark(): void;
 }
-
 
 // 1. 根据值的类型断言
 function testAnimal(animal: IBird | IDog) {
   if (animal.fly) {
-    (animal as IBird).sing()
+    (animal as IBird).sing();
   } else {
-    (animal as IDog).bark()
+    (animal as IDog).bark();
   }
 }
 
-
 // 2. in 语法保护 (不需要写类型断言)
 function testAnimal2(animal: IBird | IDog) {
-  if ('sing' in animal) {
-    animal.sing()
+  if ("sing" in animal) {
+    animal.sing();
   } else {
-    animal.bark()
+    animal.bark();
   }
 }
 
 // 3. typeof 类型判断
 function add1(x: number | number[], y?: number): number {
-  if (typeof x === 'number' && typeof y === 'number') {
-    return x + y
+  if (typeof x === "number" && typeof y === "number") {
+    return x + y;
   }
-  return (x as number[]).reduce((pre, cur) => pre + cur)
+  return (x as number[]).reduce((pre, cur) => pre + cur);
 }
 
 console.log(add1([1, 2, 3, 4])); // 10
 
 console.log(add1(1, 4)); // 5
 
-
 // 4. instanceof
 
 class NumObj {
-  constructor(public count: number = 0) { }
+  constructor(public count: number = 0) {}
 }
 
 function add2(x: number | NumObj, y: number | NumObj) {
@@ -61,11 +58,9 @@ function add2(x: number | NumObj, y: number | NumObj) {
   return (x as number) + (y as number);
 }
 
-console.log(add2(1, 2)) // 3
+console.log(add2(1, 2)); // 3
 
-console.log(add2(new NumObj(2), new NumObj(4))) // 16
-
-
+console.log(add2(new NumObj(2), new NumObj(4))); // 16
 
 // 5.自定义类型保护的类型谓词
 
@@ -77,14 +72,63 @@ function isString(x: any): x is string {
   return typeof x === "string";
 }
 
-function example(foo: any){
-  if(isString(foo)){
-      console.log("it is a string" + foo);
-      console.log(foo.length); // string function
-      // 如下代码编译时会出错，运行时也会出错，因为经过了类型判断后 foo 是 string 不存在toExponential方法
-      // console.log(foo.toExponential(2));
+function example(foo: any) {
+  if (isString(foo)) {
+    console.log("it is a string" + foo);
+    console.log(foo.length); // string function
+    // 如下代码编译时会出错，运行时也会出错，因为经过了类型判断后 foo 是 string 不存在toExponential方法
+    // console.log(foo.toExponential(2));
   }
   // 编译不会出错，但是运行时出错
   console.log(foo.toExponential(2));
 }
 example("hello world");
+
+// 类型保护谓词 - 用例 2
+interface Square {
+  kind: "square";
+  size: number;
+}
+interface Rectangle {
+  kind: "rectangle";
+  width: number;
+  height: number;
+}
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+type Shape = Square | Rectangle | Circle;
+class ShapeLike {
+  isSquare(type: Shape): type is Square {
+    return (type as Square).size !== undefined;
+  }
+  isRectangle(type: Shape): type is Rectangle {
+    return (type as Rectangle).width !== undefined;
+  }
+  isCircle(type: Shape): type is Circle {
+    return (type as Circle).radius !== undefined;
+  }
+  getArea(type: Shape) {
+    if (this.isSquare(type)) {
+      return type.size * 2;
+    }
+    // 直接使用属性值判断也是可以的
+    if (type.kind === "rectangle") {
+      return type.width * type.height;
+    }
+    if (type.kind === "circle") {
+      return type.radius ** 2 * Math.PI;
+    }
+  }
+}
+
+let sp = new ShapeLike();
+
+let sq: Circle = {
+  kind: "circle",
+  radius: 5,
+};
+let area = sp.getArea(sq);
+
+console.log(area);
---
+++ ts-learn/02-yarn+ts+webpack/src/07装饰器/decorator02.ts
@@ -0,0 +1,87 @@
+// 设想有这样一个场景。
+// 目前有一个Tank类，有一个Plane类，有一个Animal类。这三个类都需要一个公共的方法来获取他们所在的位置。我们第一可能想到使用继承来实现。
+
+class BaseClass {
+  getPosition() {
+    return {
+      x: 100,
+      y: 200,
+      z: 300,
+    };
+  }
+}
+// class Tank extends BaseClass {}
+// class Plane extends BaseClass {}
+// class Animal extends BaseClass {}
+
+/*
+  这样三个类都可以调用getPosition方法来获取各自的位置了。到目前为止看起来没什么问题。
+  现在又有了一个新的诉求，Tank 类和Plane类需要一个新的方法addPetrol来给坦克和飞机加油。而动物不需要加油。此时这种写法好像不能继续进行下去了。而js目前没有直接语法提供多继承的功能，我们的继承方式好像行不通了。这时候装饰器可以很完美的实现这样的功能。此时就可以请我们的装饰器闪亮登场了~
+*/
+
+// 装饰器功能之 —— 能力扩展
+// 我们把getPosition和addPertrol都抽象成一个单独的功能，它们得作用是给宿主扩展对应的功能。
+
+const getPosDecorator: ClassDecorator = (constructor: Function) => {
+  constructor.prototype.getPosition = () => [100, 200];
+};
+const addPetroDecorator: ClassDecorator = (constructor: Function) => {
+  constructor.prototype.addPetro = function () {
+    return `${this.name}进行加油`;
+  };
+};
+
+@getPosDecorator
+@addPetroDecorator
+class Tank {
+  [x: string]: any;
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+@getPosDecorator
+@addPetroDecorator
+class Plane {
+  [x: string]: any;
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+@getPosDecorator
+class Animal {
+  [x: string]: any;
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+// 多个装饰器叠加的时候，执行顺序为离被装饰对象越近的装饰器越先执行。
+// 注意不要被覆写了
+
+console.log(new Plane("飞机").getPosition()); // [ 100, 200 ]
+
+console.log(new Plane("飞机").addPetro()); // 飞机进行加油
+
+// console.log(new Animal("老虎")?.addPetro()); // TypeError: (intermediate value).addPetro is not a function
+
+
+
+
+
+
+/* 属性装饰器 */
+
+const initCarPropertyDec = <T>(property: T) => {
+  return (target: object, propertyKey: string | symbol) => {
+    target[propertyKey] = property;
+  };
+};
+
+class Car {
+  @initCarPropertyDec("奔驰")
+  name!: string;
+}
+
+console.log(new Car().name);

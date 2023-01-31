ea9b2dff6d284336827083f2719a0d1a4e11ef93
zndkqiu <zndkqiu@tencent.com>
2023-01-31 15:54:39 +0800

    update: docker-learn

--- docker-learn/index.js
+++ docker-learn/index.js
@@ -2,8 +2,10 @@ const koa = require("koa");
 const app = new koa();
 
 app.use(async (ctx) => {
-  console.log(`User from ${ctx.request.host}`);
+  console.log(`User from: ${ctx.request.origin}`);
+  console.log(`User requesting: ${ctx.request.host}`);
   ctx.body = "Great, you access the docker-image!";
+  ctx.status = 200;
 });
 
 app.listen(80, () => {

c2c4dd9eeefd2ce0c79103e4e04dcaa81aaaa6ec
zndkqiu <zndkqiu@tencent.com>
2025-06-18 14:39:03 +0800

    doc: update linux part

--- WebProjects/zn-nuxt-app/components/foo.vue
+++ WebProjects/zn-nuxt-app/components/foo.vue
@@ -1,5 +1,28 @@
 <template>
   <div>
     <h1>This is Foo Component</h1>
+
+    <HighLink href="#" target="_blank">
+      Link
+    </HighLink>
   </div>
 </template>
+
+<script setup lang="ts">
+import { h, type FunctionalComponent } from 'vue';
+
+const HighLink: FunctionalComponent = (props, ctx) => {
+  return h('a',
+    {
+      style: {
+        fontWeight: 500,
+        color: 'pink',
+        textDecoration: 'none'
+      },
+      ...props,
+    },
+    ctx.slots?.default?.(),
+  )
+}
+
+</script>
